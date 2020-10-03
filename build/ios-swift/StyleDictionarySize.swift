e8e49eafd7aba8dcc1b9b94de1b5a316b93fa762
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-10-03 23:05:05 +0200

    add style dictionary

---
+++ build/ios-swift/StyleDictionarySize.swift
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.swift
+//
+// Do not edit directly
+// Generated on Sat, 03 Oct 2020 21:04:37 GMT
+//
+
+import UIKit
+
+public enum StyleDictionarySize {
+    public static let fontBase = CGFloat(16.00)
+    public static let fontLarge = CGFloat(32.00)
+    public static let fontMedium = CGFloat(16.00)
+    public static let fontSmall = CGFloat(12.00)
+}
---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+// Do not edit directly
+// Generated on Sat, 03 Oct 2020 21:04:37 GMT
+//
+
+#import "StyleDictionarySize.h"
+
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
