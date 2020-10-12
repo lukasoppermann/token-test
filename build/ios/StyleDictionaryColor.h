75877985b648e4f01ef1450d2ff04917c08267d9
Automated Publisher <actions@users.noreply.github.com>
2020-10-12 19:36:33 +0000

    Automated publish: Mon Oct 12 19:36:33 UTC 2020 217f63fccdffbd1f1a3b0c2f466df460f7e86504

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.h
 //
 // Do not edit directly
-// Generated on Sun, 04 Oct 2020 21:13:54 GMT
+// Generated on Mon, 12 Oct 2020 19:36:33 GMT
 //
 
 #import <UIKit/UIKit.h>
@@ -14,27 +14,9 @@ BordersUnsupportedMultipleBordersStroke,
 BordersSingleStroke,
 BordersSingleStyleStroke,
 BordersDashedOutsideStroke,
-ColorsMultipleFills0,
-ColorsMultipleFills1,
-ColorsSingleBlue,
-ColorsSpecialCharacters😅,
-ColorsSpecialCharactersAnderung,
-GradientMultiple0Stops0Color,
-GradientMultiple0Stops1Color,
-GradientMultiple1Stops0Color,
-GradientMultiple1Stops1Color,
-GradientMultiple2Stops0Color,
-GradientMultiple2Stops1Color,
-GradientMultiple3Stops0Color,
-GradientMultiple3Stops1Color,
-GradientSingleWithMultipleColorStopsStops0Color,
-GradientSingleWithMultipleColorStopsStops1Color,
-GradientSingleWithMultipleColorStopsStops2Color,
-GradientSingleWithMultipleColorStopsStops3Color,
-EffectDropShadowSingleColor,
-EffectInnerShadowMultiple0Color,
-EffectInnerShadowMultiple1Color,
-EffectInnerShadowMultiple2Color
+InvalidOrange,
+TokencolorValid,
+TokencolorInvalid
 };
 
 @interface StyleDictionaryColor : NSObject
--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Sun, 04 Oct 2020 21:13:54 GMT
+// Generated on Mon, 12 Oct 2020 19:36:33 GMT
 //
 
 #import "StyleDictionaryColor.h"
@@ -25,27 +25,9 @@ rgba(255, 230, 0, 1),
 rgba(112, 141, 242, 1),
 rgba(4, 74, 255, 1),
 rgba(64, 255, 186, 1),
-rgba(64, 255, 186, 1),
-rgba(0, 0, 0, 0.1),
-rgba(4, 74, 255, 1),
-rgba(64, 223, 80, 1),
-rgba(52, 86, 175, 1),
-rgba(255, 184, 0, 1),
-rgba(255, 184, 0, 1),
-rgba(255, 255, 255, 1),
-rgba(255, 255, 255, 1),
-rgba(207, 48, 48, 1),
-rgba(255, 255, 255, 1),
-rgba(74, 79, 204, 1),
-rgba(255, 255, 255, 1),
-rgba(255, 184, 0, 1),
-rgba(255, 138, 0, 1),
-rgba(255, 46, 0, 1),
-rgba(255, 0, 0, 1),
-rgba(0, 0, 0, 0.25),
-rgba(0, 0, 0, 0.25),
-rgba(0, 0, 0, 1),
-rgba(0, 0, 0, 0.25)
+rgba(255, 128, 64, 1),
+rgba(66, 231, 132, 1),
+rgba(254, 80, 80, 1)
     ];
   });
 
