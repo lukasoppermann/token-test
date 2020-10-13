4c58e12a96144b48d8c8f8a3416490a1ba894abc
Automated Publisher <actions@users.noreply.github.com>
2020-10-13 11:01:47 +0000

    Automated publish: Tue Oct 13 11:01:47 UTC 2020 947eb36d0977a5e9e767d88062121107ca5685cd

--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Tue, 13 Oct 2020 10:25:57 GMT
+// Generated on Tue, 13 Oct 2020 11:01:47 GMT
 //
 
 #import "StyleDictionaryColor.h"
